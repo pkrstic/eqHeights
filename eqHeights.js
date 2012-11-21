5cced350f1f306edf9d37886113cb34ce5b7479e
Predrag Krstić <krstic.p@gmail.com>
2012-11-21 10:45:26 +0100

    updated comments

--- eqHeights.js
+++ eqHeights.js
@@ -1,10 +1,13 @@
-/*
- *	Plugin name: eqHeights
- *	Author: Predrag Krstic
- *	License: Creative Commons Attribution-ShareAlike 3.0 Unported License
- *           http://creativecommons.org/licenses/by-sa/3.0/
- *	Copyright (c) 2010
- *	Version: 1.0
+/**
+ * Tabs jQuery plugin
+ * https://github.com/pkrstic/eqHeights
+ *
+ * Copyright 2012, Predrag Krstic
+ * Licensed under the MIT license.
+ * see LICENSE.txt
+ *
+ * @author Predrag Krstic
+ * @verson 1.0
  */
 (function($){
 $.fn.extend({ 
@@ -16,9 +19,8 @@ $.fn.extend({
 			var h = $(this).height();
 			if (h > maxH) maxH = h;
 		});
-		return this.each(function() {
-			$(this).height(maxH);
-		});
+
+		return $(this).height(maxH);
 	}
 });
 })(jQuery);
